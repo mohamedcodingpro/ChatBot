093c480877294b934b1336875e3cc6e049179043
MotlalepulaMakhanya <motlalepulamakhanya5@gmail.com>
2024-09-18 12:33:11 +0200

    See

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to 0.0.0.0 to accept external requests
+    port: process.env.PORT || 3000, // Use Render's PORT or fallback to 3000
+  }
 })
+
